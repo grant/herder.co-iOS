1e311ed891f863dfe963acea1806cb89fbd21be3
Grant Timmerman <granttimmerman@gmail.com>
2014-09-06 19:43:28 -0700

    init iOS app

---
+++ herder.co iOS/herder.co iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  herder.co iOS
+//
+//  Created by Grant Timmerman on 9/6/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
